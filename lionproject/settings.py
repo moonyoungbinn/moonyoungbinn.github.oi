799f6593d32953a2123a764e6b64603f1bb1ceb6
Your Name <you@example.com>
2021-06-28 16:01:08 +0900

    for heroku

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -154,8 +155,6 @@ AWS_STORAGE_BUCKET_NAME = 'likelion-django-bucket'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'ap-northeast-2'
 AWS_S3_CUSTOM_DOMAIN = ''
-
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
